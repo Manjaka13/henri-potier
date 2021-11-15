b4b424a1790c6d760673f9e8bb3a10532a1fea94
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-11-15 16:45:04 +0100

    add pages

--- helpers/interface.ts
+++ helpers/interface.ts
@@ -14,7 +14,7 @@ export interface IButtonProps {
 	type?: string;
 	title?: string;
 	children?: string;
-	onClick?: any;
+	onClick?: Function;
 }
 
 // Heading component props
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-import Head from "next/head";
+import Page from "components/Page";
+import Book from "components/Book";
+import books from "helpers/book-mocky.json";
+import { v4 as uuidv4 } from "uuid";
+import {IBook} from "helpers/interface";
 
-export default function Home() {
+/*
+	Home page
+*/
+
+const Home = (): JSX.Element => {
+	// Map book list
+	const mappedBooks: Array<JSX.Element> = books.map((item: IBook) => (
+		<Book book={item} key={uuidv4()} />
+	));
+
 	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main>
-				<p>test</p>
-			</main>
-		</div>
+		<Page
+			title="Les livres d'Henri Potier"
+			description="Parcourez la liste des livres d'Henri Potier à prix abordables."
+			active={0}
+		>
+			<div className="container book-list">
+				{mappedBooks}
+			</div>
+		</Page>
 	);
 }
+
+export default Home;
