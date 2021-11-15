7484fb9332fe5620484b39969e4bf5bdcb3a275a
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-11-15 20:46:30 +0100

    pre commit format

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import {IBadge} from "helpers/interface";
+import { IBadge } from "helpers/interface";
 
-const Badge: React.FC<IBadge> = ({number}): JSX.Element => {
-	if(number && number > 0)
+const Badge: React.FC<IBadge> = ({ number }): JSX.Element => {
+	if (number && number > 0)
 		return (
 			<div className="badge">
-				<p className="badge__number">
-					{number >= 100  ? "+99" : number}
-				</p>
+				<p className="badge__number">{number >= 100 ? "+99" : number}</p>
 			</div>
 		);
 	return null;
-}
+};
 
 export default Badge;
